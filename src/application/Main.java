3f38912dbcb32d489875b7b9a727d2df4179477b
joao victor benedet machado <jvbm2177@gmail.com>
2025-02-09 15:10:01 -0300

    findByDepartment

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,9 @@ import model.dao.impl.SellerDAOJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,8 +25,11 @@ public class Main {
         Department dep = depdao.findById(3);
         System.out.println(dep);
 
-
-
-
+        System.out.println(" =*= TEST N2: seller findByDepartment  =*= ");
+        Department depart = new Department(2, null);
+        List<Seller> listSellers = sellerdao.findByDepartment(depart);
+        for (Seller obj : listSellers) {
+            System.out.println(obj);
+        }
     }
 }
